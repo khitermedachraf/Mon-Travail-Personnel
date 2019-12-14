74e993ae1c6b1cb92f9eb0797b5a1dc6e2d2cbc6
khitermedachraf <hm_khiter@esi.dz>
2019-12-14 21:24:42 +0100

    add some algorithms with python as a begining

---
+++ Mon travail sur Python/chiffrement_Algo RSA.py
@@ -0,0 +1,15 @@
+# L'utilisateur entre p
+p = input('Entrez un grand nombre premier p : ')
+
+# L'utilisateur entre q
+q = input('Entrez un grand nombre premier q : ')
+
+# On calcule n 
+n = p*q
+
+print "\nn = ",n,
+
+# On calcul phi(n)
+phiden = (p-1)*(q-1)
+
+print "\nphi de n = ",phiden,
